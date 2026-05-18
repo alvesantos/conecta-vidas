17f302abb84f2394857d5e2ee14fabf05508975e
ebagabee <alvesantos.gh@gmail.com>
2026-05-18 16:52:23 -0300

    feat: remove sidebar in mobile

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="flex min-h-screen">
-    <AppSidebar />
-    <main class="flex-1 p-10">
+  <div class="flex flex-col min-h-screen md:flex-row">
+    <AppTopbar />
+    <AppSidebar class="hidden md:flex" />
+    <main class="flex-1 p-6 md:p-10">
       <slot />
     </main>
   </div>
