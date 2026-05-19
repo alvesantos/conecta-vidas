11422a6a4f467f2f6e44d96b3b8d330760850ac0
ebagabee <alvesantos.gh@gmail.com>
2026-05-19 17:29:29 -0300

    chore: remove recoletta and add inter

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,24 +1,27 @@
 <script setup lang="ts">
-import subscriptionAd from '~/layouts/subscriptionAd.vue';
+import subscriptionAd from "~/layouts/subscriptionAd.vue";
 
 definePageMeta({
-  noPadding: true
-})
+  noPadding: true,
+});
 </script>
 
 <template>
   <div class="relative">
     <header>
-      <img 
+      <img
         class="max-w-[50%] md:max-w-[20%] pt-6 px-6 md:pt-10 hidden md:block absolute"
         src="/conectavet-extended-icon.png"
-        alt="conectavet" />
+        alt="conectavet"
+      />
     </header>
 
     <div>
       <section class="flex w-full justify-between">
-      <!-- SIDE CONTENT -->
-        <div class="lg:max-w-[30%] max-w-[50%] max-h-full lg:pt-[150px] lg:px-4 flex flex-col justify-between">
+        <!-- SIDE CONTENT -->
+        <div
+          class="lg:max-w-[30%] max-w-[50%] max-h-full lg:pt-[150px] lg:px-4 flex flex-col justify-between"
+        >
           <div class="side-content pt-2 pl-2">
             <!-- TITLE -->
             <p class="text-lg lg:text-5xl">
@@ -28,17 +31,22 @@ definePageMeta({
 
             <!-- DESCRIPTION -->
             <p class="lg:text-lg text-[.6rem] lg:pt-[2rem] pt-[1.5rem]">
-              Telemedicina veterinária com <span class="text-accent font-semibold">orientação, acolhimento e segurança</span> quando você mais precisar.
+              Telemedicina veterinária com
+              <span class="text-accent font-semibold"
+                >orientação, acolhimento e segurança</span
+              >
+              quando você mais precisar.
             </p>
           </div>
         </div>
-        
+
         <!-- DOG BACKGROUND -->
         <div class="lg:max-w-[70%] max-w-[50%] shrink-0 overflow-hidden">
           <img
             class="conectavet-dog w-full h-full object-cover object-[65%_center] scale-130 lg:scale-100"
             src="/cute_dog.png"
-            alt="doggo" />
+            alt="doggo"
+          />
         </div>
       </section>
     </div>
@@ -55,30 +63,21 @@ definePageMeta({
 <style scoped>
 .side-content {
   & > :first-child {
-    font-family:
-      "Recoleta", "DM Serif Display", Georgia, serif;
-    font-weight: 600;
+    font-weight: 800;
   }
 }
 
 /* Desktop */
 @media (min-width: 1024px) {
   .conectavet-dog {
-    mask-image:
-      linear-gradient(
-        to left,
-        white 0%,
-        white 76%,
-        transparent 96%
-      );
+    mask-image: linear-gradient(to left, white 0%, white 76%, transparent 96%);
 
-    -webkit-mask-image:
-      linear-gradient(
-        to left,
-        white 0%,
-        white 76%,
-        transparent 96%
-      );
+    -webkit-mask-image: linear-gradient(
+      to left,
+      white 0%,
+      white 76%,
+      transparent 96%
+    );
 
     mask-composite: intersect;
     -webkit-mask-composite: source-in;
@@ -88,21 +87,14 @@ definePageMeta({
 /* Mobile */
 @media (max-width: 1023px) {
   .conectavet-dog {
-    mask-image:
-      linear-gradient(
-        to left,
-        white 0%,
-        white 60%,
-        transparent 85%
-      );
+    mask-image: linear-gradient(to left, white 0%, white 60%, transparent 85%);
 
-    -webkit-mask-image:
-      linear-gradient(
-        to left,
-        white 0%,
-        white 60%,
-        transparent 85%
-      );
+    -webkit-mask-image: linear-gradient(
+      to left,
+      white 0%,
+      white 60%,
+      transparent 85%
+    );
   }
 }
 </style>
