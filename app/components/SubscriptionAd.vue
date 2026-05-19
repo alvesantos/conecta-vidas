b5261ff35e7cc1c5bc37ab631987bfc3e4d15cd9
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-05-19 20:06:54 -0300

    feat: add logo to mobile version

--- app/components/SubscriptionAd.vue
+++ app/components/SubscriptionAd.vue
@@ -83,7 +83,7 @@
 
                 <p class="pt-3 text-white/80 leading-snug text-sm">
                   Saber agir quando seu filho de quatro patas
-                  mais precisar pode custar apenas 1 real por dia!
+                  mais precisar pode custar menos de dois reais por dia!
                 </p>
               </div>
             </div>
