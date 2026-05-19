3589dee0c3bfd822b6b6213c1614bba24c5d9b92
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-05-19 20:47:17 -0300

    feat: finished StoreAdvantages

--- app/components/StoreAdvantages.vue
+++ app/components/StoreAdvantages.vue
@@ -1,10 +1,34 @@
 <script lang="ts" setup>
-const advantageColumns = [
-  '/dot_k_check_shield.png',
-  '/dot_k_clock.png',
-  '/dot_k_heart.png',
-  '/dot_k_house.png',
+const advantageIcons = [
+  {
+    icon: '/dot_k_icons/dot_k_clock.png',
+    label: 'Atendimento rápido'
+  },
+  {
+    icon: '/dot_k_icons/dot_k_house.png',
+    label: 'Sem sair de casa'
+  },
+  {
+    icon: '/dot_k_icons/dot_k_check_shield.png',
+    label: 'Orientação segura'
+  },
+  {
+    icon: '/dot_k_icons/dot_k_heart.png',
+    label: 'Mais cuidado, menos preocupação'
+  }
 ]
+
+const AdvantageItem = (props: { icon: string, label: string }) =>
+  h('div', {
+    class: 'bg-foreground p-4 text-center text-accent'
+  }, [
+    h('img', {
+      src: props.icon,
+      class: 'mx-auto mb-4 w-16 h-16 object-contain'
+    }),
+
+    h('p', {}, props.label)
+  ])
 </script>
 
 <template>
@@ -18,28 +42,28 @@ const advantageColumns = [
         loop
         dots
         :autoplay="{ delay: 2000 }"
-        :items="advantageColumns"
+        :items="advantageIcons"
         :ui="{ 
           item: 'basis-full',
           dot: 'data-[state=active]:bg-accent size-3'
         }"
       >
-        <div class="bg-foreground p-4 text-center">
-          {{ item }}
-        </div>
+        <AdvantageItem
+          :icon="item.icon"
+          :label="item.label"
+        />
       </UCarousel>
 
       <div class="text-transparent lg:hidden">a</div>
 
       <!-- DESKTOP ==================== -->
       <div class="hidden lg:grid grid-cols-4 gap-px bg-gray-300">
-        <div
-          v-for="item in advantageColumns"
-          :key="item"
-          class="bg-foreground p-4 text-center"
-        >
-          {{ item }}
-        </div>
+        <AdvantageItem
+          v-for="item in advantageIcons"
+          :key="item.label"
+          :icon="item.icon"
+          :label="item.label"
+        />
       </div>
     </UCard>
   </section>
