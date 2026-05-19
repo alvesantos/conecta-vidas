59e973eece27f3da9c67b595badf97236b0ba2a5
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-05-19 16:02:43 -0300

    feat: finish card

--- app/layouts/subscriptionAd.vue
+++ app/layouts/subscriptionAd.vue
@@ -5,14 +5,46 @@
   <div class="px-2 lg:px-6">
     <!-- DESKTOP -->
     <div class="flex justify-between hidden lg:flex">
-      <div class="bg-red-500">hi</div>
-      <div class="bg-yellow-500">hey</div>
+      <!-- CARD -->
+      <div class="max-w-[30%] pb-1">
+        <UCard 
+          :ui="{body: 'bg-accent'}"
+          >
+          <div class="flex items-center gap-4">
+            <!-- ICON -->
+            <div class="shrink-0">
+              <img
+                class="w-20 h-20 object-contain"
+                src="/dot_k_icons/dot_k_pet_shield.png"
+                alt="dog-shield"
+              />
+            </div>
+
+            <!-- CONTENT -->
+            <div class="min-w-0">
+              <h3 class="text-2xl font-semibold leading-tight text-foreground">
+                Assine agora e tenha segurança!
+              </h3>
+
+              <p class="pt-3 text-white/80 leading-snug">
+                Saber agir quando seu filho de quatro patas
+                mais precisar pode custar apenas 1 real por dia!
+              </p>
+            </div>
+          </div>
+        </UCard>
+      </div>
+
+      <!-- CHIP -->
+      <div class="text-white">
+        <UCard />
+      </div>
     </div>
 
     <!-- MOBILE -->
      <div class="flex justify-between lg:hidden">
-      <div class="bg-red-500">hi mobile</div>
-      <div class="bg-yellow-500">hey mobile</div>
+      <div class="">hi mobile</div>
+      <div class="">hey mobile</div>
      </div>
   </div>
 </template>
