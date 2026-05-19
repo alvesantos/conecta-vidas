f3bec6cd264c3400707786977c1e630eac9f857f
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-05-18 21:52:14 -0300

    feat: dog mask
    making the photo of the dog with a fade-in gradient for a mask

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,38 @@
 
 <template>
   <header>
-    <img class="max-w-[50%] md:max-w-[20%]" src="/conectavet-extended-icon.png" alt="conectavet">
-    <img class="absolute right-0 top-0 max-h-[28vh] md:max-h-[720px]" src="/cute_dog.png" alt="doggo" />
+    <img 
+      class="max-w-[50%] md:max-w-[20%]"
+      src="/conectavet-extended-icon.png"
+      alt="conectavet" />
+    <div class="absolute right-0 top-0">
+      <img 
+        class="conectavet-dog max-h-[28vh] md:max-h-[720px]"
+        src="/cute_dog.png"
+        alt="doggo" />
+    </div>
   </header>
 </template>
+
+<style scoped>
+.conectavet-dog {
+  mask-image:
+    linear-gradient(
+      to left,
+      white 0%,
+      white 70%,
+      transparent 100%
+    );
+
+  -webkit-mask-image:
+    linear-gradient(
+      to left,
+      white 0%,
+      white 70%,
+      transparent 100%
+    );
+
+  mask-composite: intersect;
+  -webkit-mask-composite: source-in;
+}
+</style>
