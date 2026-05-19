6475a3c06c25e2c1122bb424192a919d98b52aaf
ebagabee <alvesantos.gh@gmail.com>
2026-05-19 17:24:25 -0300

    chore: update color mode to false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
-  colorMode: false,
+  ui: {
+    colorMode: false,
+  },
   app: {
     head: {
       title: "ConectaVet - Telemedicina Veterinária",
