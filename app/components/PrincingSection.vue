261e76c936a6c75a1795a30b83f56cd5b4d0044c
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-05-19 21:53:04 -0300

    feat: finished main layout

--- app/components/PrincingSection.vue
+++ app/components/PrincingSection.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>pricing will go here</div>
+  <div>
+    <UCard class="p-4">
+      <h2 class="text-2xl font-semibold text-primary text-center mb-4">Assine e tenha acesso a conteúdos exclusivos de primeiros socorros e cuidados com seu pet.</h2>
+
+      <p>do something here</p>
+    </UCard>
+  </div>
 </template>
