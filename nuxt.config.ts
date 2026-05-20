0fbe55b261e29856365b4c3392090a47c0abdcf1
ebagabee <alvesantos.gh@gmail.com>
2026-05-19 21:49:05 -0300

    feat: add cors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE ?? 'http://localhost:3001/api',
+    },
+  },
   devtools: { enabled: true },
   modules: ["@nuxt/ui"],
   ui: {
