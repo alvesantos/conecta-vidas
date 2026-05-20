0063ee828e2cb130e9bed482d960143463b56365
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-05-20 19:06:10 -0300

    feat: add essencial icons

--- app/components/PrincingSection.vue
+++ app/components/PrincingSection.vue
@@ -17,15 +17,15 @@ const plans: Plan[] = [
     focus_desc: "Tutores que buscam cuidado eficiente, orientação confiável e praticidade no dia a dia do seu pet.",
     price: 39.90,
     perks: [
-      { icon: "", label: "Cadastro de até 2 pets (cães e gatos)" },
-      { icon: "", label: "Teletriagem Veterinária ILIMITADA (24h)" },
-      { icon: "", label: "Consulta Virtual Agendada: 1 por mês" },
-      { icon: "", label: "Plataforma de Conteúdo: Acesso completo incluso (Vídeos educativos, culinária e mais!)" },
-      { icon: "", label: "Clube ConectaVet (Grupo VIP no WhatsApp)" },
-      { icon: "", label: "Clube de Benefícios: Descontos Exclusivos na Boutique MAFFY" },
-      { icon: "", label: "Benefício Aniversário ConectaVet: Surpresa especial para o seu pet, com cupom, parabéns personalizado e mais!" },
-      { icon: "", label: "Envio de 1 Mini Kit Emergência Pet (Anual)" },
-      { icon: "", label: "Consultas Virtuais Extras: Coparticipação inteligente (50% de desconto sobre o valor avulso)" }
+      { icon: "i-ph-dog", label: "Cadastro de até 2 pets (cães e gatos)" },
+      { icon: "i-ph-headset", label: "Teletriagem Veterinária ILIMITADA (24h)" },
+      { icon: "i-ph-stethoscope", label: "Consulta Virtual Agendada: 1 por mês" },
+      { icon: "i-ph-play-circle", label: "Plataforma de Conteúdo: Acesso completo incluso (Vídeos educativos, culinária e mais!)" },
+      { icon: "i-ph-users-three", label: "Clube ConectaVet (Grupo VIP no WhatsApp)" },
+      { icon: "i-ph-gift", label: "Clube de Benefícios: Descontos Exclusivos na Boutique MAFFY" },
+      { icon: "i-ph-gift", label: "Benefício Aniversário ConectaVet: Surpresa especial para o seu pet, com cupom, parabéns personalizado e mais!" },
+      { icon: "i-ph-gift", label: "Envio de 1 Mini Kit Emergência Pet (Anual)" },
+      { icon: "i-ph-seal-percent", label: "Consultas Virtuais Extras: Coparticipação inteligente (50% de desconto sobre o valor avulso)" }
     ],
   },
 
@@ -37,9 +37,9 @@ const plans: Plan[] = [
     focus_desc: "Tutores que desejam mais suporte, comodidade e benefícios para cuidar melhor do seu pet.",
     price: 79.90,
     perks: [
-      { icon: "", label: "Tudo incluso no Essencial" },
-      { icon: "", label: "Teletriagem Veterinária ILIMITADA (24h) com Prioridade" },
-      { icon: "", label: "Consulta Virtual Agendada ou Pronto Atendimento: 2 por mês" },
+      { icon: "i-ph-exclamation-mark", label: "Tudo incluso no Essencial" },
+      { icon: "i-ph-headset", label: "Teletriagem Veterinária ILIMITADA (24h) com Prioridade" },
+      { icon: "i-ph-stethoscope", label: "Consulta Virtual Agendada ou Pronto Atendimento: 2 por mês" },
       { icon: "", label: "Envio de 1 Mini Kit Emergência Pet: Anual" },
       { icon: "", label: "Atendimento Veterinário Domiciliar: 5% de desconto em todos os atendimentos domiciliares" },
       { icon: "", label: "Exames Laboratoriais e de Imagem em Casa: Acesso a valores reduzidos de rede" },
@@ -56,9 +56,9 @@ const plans: Plan[] = [
     focus_desc: "Tutores que querem o melhor em exclusividade, benefícios e mimos para seu pet.",
     price: 149.90,
     perks: [
-      { icon: "", label: "Tudo incluso no Care Plus e Essencial" },
-      { icon: "", label: "Teletriagem Veterinária ILIMITADA (24h) com Prioridade máxima" },
-      { icon: "", label: "Consulta Virtual ou Pronto Atendimento: 4 por mês (inclui pets exóticos/silvestres)" },
+      { icon: "i-ph-exclamation-mark", label: "Tudo incluso no Care Plus e Essencial" },
+      { icon: "i-ph-headset", label: "Teletriagem Veterinária ILIMITADA (24h) com Prioridade máxima" },
+      { icon: "i-ph-stethoscope", label: "Consulta Virtual ou Pronto Atendimento: 4 por mês (inclui pets exóticos/silvestres)" },
       { icon: "", label: "Benefício Aniversário ConectaVet: Kit premium aniversário com mimos físicos, cupom exclusivo, parabéns personalizado e descontos de elite" },
       { icon: "", label: "MAFFYBOX: Envio mensal de brinquedos, petiscos e itens de moda pet ergonômica direto para sua casa" },
       { icon: "", label: "Coleta de Exames Laboratoriais em Domicílio: 100% inclusa (até 2x/ano para Check-up de Rotina)" },
@@ -76,6 +76,7 @@ const plans: Plan[] = [
   <div>
     <UCard class="p-4">
       <h4 class="lg:block hidden lg:text-2xl font-semibold text-primary text-center mb-4">Assine e tenha acesso a conteúdos exclusivos de primeiros socorros e cuidados com seu pet.</h4>
+      <h4 class="block lg:hidden text-2xl font-semibold text-primary text-center mb-4">Assine já!</h4>
 
       <section class="py-8 lg:py-16">
         <div class="grid lg:grid-cols-3 gap-6">
@@ -111,8 +112,15 @@ const plans: Plan[] = [
                 <!-- Perks -->
                 <ul class="space-y-2">
                   <li v-for="perk in plan.perks" :key="perk.label" class="flex items-center gap-2">
-                    <!-- Icon placeholder -->
-                    <UIcon v-if="perk.icon" :name="perk.icon" class="size-6 text-primary" />
+                    <div 
+                      class=" rounded-full px-3 py-3 flex items-center"
+                      :style="{ backgroundColor: plan.color }"
+                    >
+                      <UIcon 
+                        v-if="perk.icon" :name="perk.icon" class="size-6"
+                        :class="plan.title === 'Essencial' ? 'text-primary' : 'text-foreground'"
+                      />
+                    </div>
                     <!-- Text -->
                     <span class="text-primary">{{ perk.label }}</span>
                   </li>
