b24632dcb1e79d73ac18ce2cdd30b66500d272de
dotKingfall <62101379+dotKingfall@users.noreply.github.com>
2026-05-20 23:20:57 -0300

    fix: info assinaturas on mobile

--- app/pages/assinaturas.vue
+++ app/pages/assinaturas.vue
@@ -5,7 +5,7 @@
   <div>
     <PrincingSection :reduced="false" />
 
-    <div class="grid grid-cols-[1fr_auto] gap-4 pt-4">
+    <div class="lg:grid lg:grid-cols-[1fr_auto] gap-4 pt-4 flex flex-col">
       <div class="bg-green-500">a</div>
       <div class="bg-red-500">b</div>
     </div>
